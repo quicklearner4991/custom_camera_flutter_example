5e46317943e6ab4c247354252576487a790e0800
Raman <raman@Ramans-MacBook-Pro.local>
2022-12-31 23:30:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:camera/camera.dart';
+import 'package:custom_camera_flutter/custom_camera_screen.dart';
+import 'package:flutter/material.dart';
+List<CameraDescription> cameras = [];
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error in fetching the cameras: $e');
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Camera Example',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: CustomCameraScreen(),
+    );
+  }
+}
+
+
